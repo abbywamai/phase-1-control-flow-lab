6347eb7775d9e57a501aed4197cb8432efc56157
abigail <abigail.wamai@student.moringaschool.com>
2023-06-13 10:21:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value) {
+  if (value <= 400) {
+    return "This one is on me!";
+  } else if (value > 2000 && value <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (value > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  return city === "NYC" ?
+     "Ok, sounds good.": "No go.";
+    
+  } 
+    
+  
+  
+
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  if(generous){
+    return "Thank you so much."
+  } else if (notAsGenerous){
+    return "Thank you."
+  } else if (anythingElse){
+    return "Bye."
+  }
+  
 }
